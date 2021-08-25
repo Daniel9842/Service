334ee3cccc93a935dc1d1bca64bcad95550604db
Daniel Ducuara <daniel.ducuara@mail.escuelaing.edu.co>
2021-08-24 23:57:18 -0500

    service js

--- src/main/resources/public/alpha.js
+++ src/main/resources/public/alpha.js
@@ -1,17 +1,17 @@
 
+const searchId = window.location.search;
+const urlParams = new URLSearchParams(searchId);
+var id = urlParams.get('identificador');
+console.log(id);
 
-function getDataService() {
-	const searchId = window.location.search;
-	const urlParams = new URLSearchParams(searchId);
-	var id = urlParams.get('identificador');
-	console.log(id);
+const requestURL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + id + '&apikey=3A320OI0SQZ06SKJ';
+fetch(requestURL)
+	.then(response => response.text())
+	.then
+	.then(data => console.log(data));
+	
 
-	const requestURL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + id + '&apikey=3A320OI0SQZ06SKJ';
-	fetch(requestURL)
-		.then(response => response.json())
-		.then(data => console.log(data));
-}
-
+	
 
 
 
